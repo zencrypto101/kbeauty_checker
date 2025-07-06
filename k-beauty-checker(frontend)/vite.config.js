32a542ab0b78e8e326bbbcee16994d6d2d2443a3
Your <you@example.com>
2025-07-06 20:30:52 +0900

    Update vite.config.js with base path and rebuild docs

--- k-beauty-checker(frontend)/vite.config.js
+++ k-beauty-checker(frontend)/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kbeauty_checker/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
